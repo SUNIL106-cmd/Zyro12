eca8c1ed75fc883273116e7bdb501c607d87a04b
https://github.com/SUNIL106-cmd/Zyro2.git <https://github.com/SUNIL106-cmd/Zyro2.git>
2025-09-05 18:53:02 +0530

    Updated Model of the Week prize details

--- script.js
+++ script.js
@@ -5,3 +5,20 @@ slider.addEventListener('animationiteration', () => {
   // first image ko end me move kar de
   slider.appendChild(slider.firstElementChild);
 });
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  document.querySelectorAll('.read-more-btn').forEach(btn => {
+    btn.addEventListener('click', function(e) {
+      e.preventDefault();
+      const fullText = this.previousElementSibling; // about-full paragraph
+      if (fullText.style.display === "block") {
+        fullText.style.display = "none";
+        this.textContent = "Read Full Story";
+      } else {
+        fullText.style.display = "block";
+        this.textContent = "Read Less";
+      }
+    });
+  });
+});
